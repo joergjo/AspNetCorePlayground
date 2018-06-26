ca5528ebd42adbceb423d477f2101371fa8d9641
Jörg Jooss (MSFT) <joerg.jooss@microsoft.com>
2018-06-26 15:10:17 +0200

    Added "WorkAccounts" sample.

---
+++ WorkAccounts/WorkAccounts/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WorkAccounts.Models;
+
+namespace WorkAccounts.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "OIDC Debug Information";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+            return View();
+        }
+
+        public IActionResult Privacy() => View();
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
